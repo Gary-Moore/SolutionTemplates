8b38fda14b225a8e2e85009b9d879db2dab1d37c
Gary Moore <garyprm@gmail.com>
2017-12-30 22:26:18 +0000

    Rename folders and files

--- MVC/MainProject.SubProject/GlobalAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyProduct("Main Project")]
-[assembly: AssemblyCompany("Gary Moore")]
-[assembly: AssemblyCopyright("Copyright © Gary Moore 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
--- MVC/MainProject.SubProject/MainProject.SubProject.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MainProject.SubProject.UI.MvcApplication" Language="C#" %>
